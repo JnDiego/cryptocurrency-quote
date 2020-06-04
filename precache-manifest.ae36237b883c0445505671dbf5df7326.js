5b001f48d3873fa84d032f5a355401072ad2ebe7
Juan Diego <diego_suarez@outlook.com>
2020-06-03 19:35:01 -0500

    Updates

---
+++ precache-manifest.ae36237b883c0445505671dbf5df7326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e645df663f614a40fdc49d1c487abb",
+    "url": "/cryptocurrency-quote/index.html"
+  },
+  {
+    "revision": "5c5d9e5f320f59eca70f",
+    "url": "/cryptocurrency-quote/static/css/main.b68f887b.chunk.css"
+  },
+  {
+    "revision": "832b5f451021d71cb63f",
+    "url": "/cryptocurrency-quote/static/js/2.6888b1ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cryptocurrency-quote/static/js/2.6888b1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5d9e5f320f59eca70f",
+    "url": "/cryptocurrency-quote/static/js/main.cb67253a.chunk.js"
+  },
+  {
+    "revision": "371068b7eff7a58ea1c0",
+    "url": "/cryptocurrency-quote/static/js/runtime-main.08c7cf49.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/cryptocurrency-quote/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cryptocurrency-quote/precache-manifest.ae36237b883c0445505671dbf5df7326.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cryptocurrency-quote/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
